5ef9e178558e7b60a4d6bd1e810d4f343d18bf8f
DevelopTech365 <cxbfd2023@gmail.com>
2024-10-12 15:20:04 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+namespace SpriteKind {
+    export const Duck = SpriteKind.create()
+}
+scene.setBackgroundColor(1)
+let mySprite = sprites.create(assets.image`Duckey`, SpriteKind.Duck)
+forever(function () {
+    mySprite.setBounceOnWall(true)
+})
